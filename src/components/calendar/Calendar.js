e93d9f1bbd73d6994674afaffc0b5b24aca5122b
Landon Sherwood <landonsherwood23@gmail.com>
2018-08-13 23:12:49 -0500

    Add calendar

--- src/components/calendar/Calendar.js
+++ src/components/calendar/Calendar.js
@@ -12,7 +12,7 @@ class Calendar extends Component {
   render() {
     return (
       <div className="calendar-view">
-        Calendar
+        <iframe src="https://calendar.google.com/calendar/b/1/embed?title=Calendar&amp;showNav=0&amp;showTabs=0&amp;showCalendars=0&amp;showTz=0&amp;height=600&amp;wkst=1&amp;bgcolor=%23FFFFFF&amp;src=89uhvp3v9i1bpa2fkoante50mis64bni%40import.calendar.google.com&amp;color=%23711616&amp;ctz=America%2FChicago" width="80%" height="95%" title="calendar" frameBorder="0" scrolling="no" className="calendar"/>
       </div>
     );
   }
